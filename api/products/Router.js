91118483165a8f6e6f6ba171309523f7c030faed
ZainabArzoo <125433716+ZainabArzoo@users.noreply.github.com>
2023-08-01 07:19:37 -1200

    Update Router.js

--- api/products/Router.js
+++ api/products/Router.js
@@ -1,13 +1,13 @@
 const app = require('express')
 const router = app.Router()
-const { AddProduct,AllProducts,UpdateProduct,DeleteProduct,ProductByBrand,ProductByCategory } = require('./Controller')
+const { AddProduct,AllProducts,UpdateProduct,DeleteProduct,ProductByName,ProductByCategory } = require('./Controller')
 
 
 router.post('/addproduct', AddProduct)
 router.get('/allproducts', AllProducts)
 router.put('/updateproduct', UpdateProduct)
 router.delete('/deleteproduct', DeleteProduct)
-router.get('/productbybrand/:brand', ProductByBrand)
+router.get('/productbyname/:name', ProductByName)
 router.get('/productbycategory/:category', ProductByCategory)
 
 module.exports = router
